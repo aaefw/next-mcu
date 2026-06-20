f49b2704f724833a8fd2c08f8fcf63201b1be360
iii <estrada.flsw@gmail.com>
2026-06-19 21:52:43 -0600

    feat: fix dynamic metadata

--- api/templates/head.php
+++ api/templates/head.php
@@ -1,19 +1,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $title; ?> - The Next MCU Movie</title>
+    <link rel="canonical" href="https://nmcu.vercel.app" />
     <link rel="shortcut icon" href="./favicon.svg" type="image/x-icon">
-    <meta name="title" content="<?= $title; ?> - The Next MCU Movie" />
+    <title><?= $title; ?> – The Next MCU Movie</title>
+    <meta name="title" content="<?= $title; ?> – The Next MCU Movie" />
     <meta name="description" content="<?= $overview ?>" />
     <meta property="og:type" content="website" />
-    <meta property="og:url" content="https://next-mcu.vercel.app/" />
-    <meta property="og:title" content="<?= $title; ?> - The Next MCU Movie" />
+    <meta property="og:url" content="https://nmcu.vercel.app" />
+    <meta property="og:title" content="<?= $title; ?> – The Next MCU Movie" />
     <meta property="og:description" content="<?= $overview ?>" />
     <meta property="og:image" content="<?= $poster_url ?>" />
-    <meta property="twitter:card" content="summary_large_image" />
-    <meta property="twitter:url" content="https://next-mcu.vercel.app/" />
-    <meta property="twitter:title" content="<?= $title; ?> - The Next MCU Movie" />
-    <meta property="twitter:description" content="<?= $overview ?>" />
-    <meta property="twitter:image" content="<?= $poster_url ?>" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:url" content="https://nmcu.vercel.app" />
+    <meta name="twitter:title" content="<?= $title; ?> – The Next MCU Movie" />
+    <meta name="twitter:description" content="<?= $overview ?>" />
+    <meta name="twitter:image" content="<?= $poster_url ?>" />
     <link rel="stylesheet" href="./style.css">
 </head>
